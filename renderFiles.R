8242ca262c0dd16c259c09d3c20737310e960468
jacpete <jacobmpeterson12@gmail.com>
2020-02-12 21:58:15 -0600

    testing bookdown references

--- renderFiles.R
+++ renderFiles.R
@@ -3,9 +3,15 @@ library(markdown)
 
 
 file.remove('pdf_document.yaml')
+# yml_empty() %>% 
+#   yml_output(pdf_document(toc = TRUE,
+#                           extra_dependencies = c('blindtext', 'color'))) %>%
+#   yml_pluck("output") %>%
+#   use_yml_file(path = 'pdf_document.yaml', git_ignore = TRUE)
 yml_empty() %>% 
-  yml_output(pdf_document(toc = TRUE,
-                          extra_dependencies = c('blindtext', 'color'))) %>%
+  yml_output(bookdown::pdf_document2(toc = FALSE,
+                          extra_dependencies = c('blindtext', 'color'),
+                          fig_caption = TRUE)) %>%
   yml_pluck("output") %>%
   use_yml_file(path = 'pdf_document.yaml', git_ignore = TRUE)
 
@@ -25,3 +31,9 @@ rmarkdown::render(input = 'Envionmental_Informatics_Project.Rmd',
                   output_yaml = 'blogdown_html_page.yaml',
                   params = list(type = "html",
                                 appendix = TRUE))
+
+
+# Figure \@ref(fig:cars-plot) is
+# ```{r cars-plot, out.width='70%', fig.cap='A figure example with a relative width 70\\%.'}
+
+# ```{r Plot-Individuals-Per-Study-Region, warning=F, results='hide', fig.align='center'}
