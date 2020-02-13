a760bbbc87d987f271020885d24a0c0b1f6bab7b
jacpete <jacobmpeterson12@gmail.com>
2020-02-13 02:58:41 -0600

    fix links in text to work with both pdf and html output

--- renderFiles.R
+++ renderFiles.R
@@ -2,28 +2,30 @@ library(ymlthis)
 library(markdown)
 
 
-file.remove('pdf_document.yaml')
+# file.remove('pdf_document.yaml')
 # yml_empty() %>% 
 #   yml_output(pdf_document(toc = TRUE,
 #                           extra_dependencies = c('blindtext', 'color'))) %>%
 #   yml_pluck("output") %>%
 #   use_yml_file(path = 'pdf_document.yaml', git_ignore = TRUE)
-yml_empty() %>% 
+file.remove('pdf_document.yaml'); yml_empty() %>% 
   yml_output(bookdown::pdf_document2(toc = FALSE,
                           extra_dependencies = c('blindtext', 'color'),
-                          fig_caption = TRUE)) %>%
+                          keep_md = TRUE,
+                          keep_tex = TRUE)) %>%
   yml_pluck("output") %>%
   use_yml_file(path = 'pdf_document.yaml', git_ignore = TRUE)
 
 rmarkdown::render(input = 'Envionmental_Informatics_Project.Rmd',
                   output_yaml = 'pdf_document.yaml',
                   params = list(type = "pdf",
-                                appendix = TRUE))
+                                appendix = FALSE))
                   
 
-file.remove('blogdown_html_page.yaml')
-yml_empty() %>% 
-  yml_output(blogdown::html_page(toc = TRUE)) %>%
+file.remove('blogdown_html_page.yaml'); yml_empty() %>% 
+  yml_output(blogdown::html_page(toc = TRUE,
+                                 fig_caption = TRUE,
+                                 keep_md = TRUE)) %>%
   yml_pluck("output") %>%
   use_yml_file(path = 'blogdown_html_page.yaml', git_ignore = TRUE)
 
